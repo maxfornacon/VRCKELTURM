976148e94d5149d37deae92bac31d70e0713d460
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2019-11-19 16:47:57 +0100

    Add Endgame state to LoseDetector

--- VRCKELTURM/Assets/LoseDetector.cs
+++ VRCKELTURM/Assets/LoseDetector.cs
@@ -37,6 +37,7 @@ public class LoseDetector : MonoBehaviour
     {
         if (IsNotColliding() || IsZRotated())
         {
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
