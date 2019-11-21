8aff8eddcebb60f1b2b529c396b4c8e7e576a0c6
maxfornacon <maximilian.fornacon@gmail.com>
2019-11-21 21:58:12 +0100

    New tower

---
+++ VRCKELTURM/Assets/Scripts/BounceDamper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceDamper : MonoBehaviour
+{
+    private float damp = 0.1f;
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity =
+            Vector3.Scale(GetComponent<Rigidbody>().velocity, new Vector3(1, 1 - damp, 1));
+    }
+}
