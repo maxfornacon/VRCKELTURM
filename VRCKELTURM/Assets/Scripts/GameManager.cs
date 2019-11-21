f73b6b00c1674951fd2f756ed976470a756f250b
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2019-11-21 11:05:23 +0100

    Add documentation

--- VRCKELTURM/Assets/Scripts/GameManager.cs
+++ VRCKELTURM/Assets/Scripts/GameManager.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// 	Class that manages game states.
+/// </summary>
 public class GameManager : MonoBehaviour {
 
 	bool gameHasEnded = false;
@@ -8,24 +11,34 @@ public class GameManager : MonoBehaviour {
 
 	public float timeUntilLoseScreen = 1f;
 
-	public void CompleteLevel ()
+	/// <summary>
+	/// 	Displays the game over screen.
+	/// </summary>
+	public void GameOver ()
 	{
 		gameOverScreen.SetActive(true);
 	}
 
+	/// <summary>
+	/// 	Ends the Game, sets it into gameHasEnded state and calls GameOver().
+	/// </summary>
 	public void EndGame ()
 	{
 		if (gameHasEnded == false)
 		{
 			gameHasEnded = true;
 			Debug.Log("GAME OVER");
-			Invoke(nameof(CompleteLevel), timeUntilLoseScreen);
+			Invoke(nameof(GameOver), timeUntilLoseScreen);
 		}
 	}
-
+	
+	/// <summary>
+	/// 	Restarts the current level.
+	/// </summary>
 	void Restart ()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		gameHasEnded = false;
 	}
 
 }
