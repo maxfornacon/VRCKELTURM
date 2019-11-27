74dbc2536f370532142d9c6c2ba886bfbb7d9210
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2019-11-27 13:39:05 +0100

    Add documentation for lose detector

--- VRCKELTURM/Assets/Scripts/LoseDetector2.cs
+++ VRCKELTURM/Assets/Scripts/LoseDetector2.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class LoseDetector2 : MonoBehaviour
 {
     private short collisionCount = 0;
-
+    
+    /// <summary>
+    /// Increases collisionCount on collision enter.
+    /// </summary>
+    /// <param name="other">the collision partner</param>
     private void OnCollisionEnter(Collision other)
     {
         if (other.transform.gameObject.CompareTag("Blocks"))
@@ -15,6 +19,10 @@ public class LoseDetector2 : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Reduces collisionCount on collision exit.
+    /// </summary>
+    /// <param name="other">The collision partner</param>
     private void OnCollisionExit(Collision other)
     {
         if (other.transform.gameObject.CompareTag("Blocks"))
@@ -22,8 +30,11 @@ public class LoseDetector2 : MonoBehaviour
             collisionCount--;
         }
     }
-
-    // Update is called once per frame
+    
+    /// <summary>
+    /// Checks if collisionCount is greater than 3. If so the game is lost and the EndGame() method of the game manager
+    /// is triggered.
+    /// </summary>
     void Update()
     {
         if (collisionCount > 3)
