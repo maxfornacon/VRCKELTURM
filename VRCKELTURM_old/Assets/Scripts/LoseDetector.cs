562b91382f611f630495296a8f8ce30acf6a9c64
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2019-12-03 14:10:21 +0100

    Downgrade project to 2018.4.12f1

---
+++ VRCKELTURM_old/Assets/Scripts/LoseDetector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class LoseDetector : MonoBehaviour
+{
+    private bool grabbed = false;
+    
+    private bool IsFalling()
+    {
+        float yVelocity = GetComponent<Rigidbody>().velocity.y;
+        if (!grabbed && yVelocity < -2)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsFalling())
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
