665d8b0055e231d637d0bedf2e07463616c9c239
maxfornacon <maximilian.fornacon@gmail.com>
2019-12-19 11:28:16 +0100

    Working PauseMenu and Pointers

---
+++ VRCKELTURM/Assets/ActivatePauseMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePauseMenu : MonoBehaviour
+{
+    public OVRInput.Button clickButton = OVRInput.Button.Start;
+    public OVRInput.Controller controller = OVRInput.Controller.LTouch;
+
+    public GameObject canvas;
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(clickButton, controller)) //check for ESC Key
+        {
+            canvas.SetActive(true);
+        }
+    }
+}
