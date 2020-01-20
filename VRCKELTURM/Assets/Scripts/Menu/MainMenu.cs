d4fd8283613ba06fc6716f5de1f5ccf5ba816570
Johann <johanngerhardt@gmx.de>
2020-01-20 20:28:21 +0100

    new Towerbuilder.cs-Structure and Settings for first two levels

--- VRCKELTURM/Assets/Scripts/Menu/MainMenu.cs
+++ VRCKELTURM/Assets/Scripts/Menu/MainMenu.cs
@@ -7,14 +7,16 @@ public class MainMenu : MonoBehaviour
 {
     public void LoadLevelOne() // Level 1
     {
-        TowerBuilder.layers = 10;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+      List<int> probability = new List<int>() {100,0,0,0,0};
+      TowerBuilder.setTowerSettings(0, 18, 4, probability, 0.98f);
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    
+
     public void LoadLevelTwo() // Level 2
     {
-        TowerBuilder.layers = 30;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+      List<int> probability = new List<int>() {20,80,0,0,0};
+      TowerBuilder.setTowerSettings(0, 24, 4, probability, 0.98f);
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
@@ -22,6 +24,6 @@ public class MainMenu : MonoBehaviour
       Debug.Log("quited game");
       Application.Quit();
     }
-    
-    
+
+
 }
