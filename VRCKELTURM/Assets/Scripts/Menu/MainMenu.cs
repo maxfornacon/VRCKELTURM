aacace0cdad8e35dbc6fc589a1bb0e37b0c6d43e
Johann <johanngerhardt@gmx.de>
2020-02-27 20:13:23 +0100

    Leveldesign erneut ueberarbeitet

--- VRCKELTURM/Assets/Scripts/Menu/MainMenu.cs
+++ VRCKELTURM/Assets/Scripts/Menu/MainMenu.cs
@@ -9,7 +9,7 @@ public class MainMenu : MonoBehaviour
     {
         List<int> probability = new List<int>() {100,0,0,0,0};
         TowerBuilder.setTowerSettings(0, 18, 4, probability, 0.98f);
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
 
@@ -17,27 +17,27 @@ public class MainMenu : MonoBehaviour
     {
         List<int> probability = new List<int>() {20,80,0,0,0};
         TowerBuilder.setTowerSettings(0, 24, 4, probability, 0.98f);
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void LoadLevelThree() // Level 3
     {
          List<int> probability = new List<int>() {20,20,20,20,20};
          TowerBuilder.setTowerSettings(0, 14, 4, probability, 0.98f);
-         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void LoadLevelFour() // Level 4
     {
         List<int> probability = new List<int>() {0,0,100,0,0};
         TowerBuilder.setTowerSettings(0, 40, 4, probability, 0.98f);
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
         Debug.Log("quited game");
-        //Application.Quit();
+        Application.Quit();
     }
 
 
