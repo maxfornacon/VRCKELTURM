829dba9ef3f82359a9e49a01f1fc61f08de8e22a
Johann <johanngerhardt@gmx.de>
2020-03-07 19:21:22 +0100

    Implement rubberband for wand and tower bricks

--- VRCKELTURM/Assets/Scripts/Rubberband.cs
+++ VRCKELTURM/Assets/Scripts/Rubberband.cs
@@ -1,41 +1,60 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using OVRTouchSample;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Rubberband : MonoBehaviour
+public class Rubberband : BaseInput
 {
     private Rigidbody pullable;
     private Rigidbody lastCollidingObject;
+
+    private OVRInput.Button clickButton = OVRInput.Button.PrimaryIndexTrigger;
 
     private float distance;
     private bool colliding;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    private void OnCollisionEnter(Collision other)
+    public override bool GetMouseButton(int button)
+    {
+        return OVRInput.Get(clickButton,  OVRInput.Controller.Touch);
+    }
+
+    public override bool GetMouseButtonDown(int button)
+    {
+        return OVRInput.GetDown(clickButton,  OVRInput.Controller.Touch);
+    }
+
+    public override bool GetMouseButtonUp(int button)
+    {
+        return OVRInput.GetUp(clickButton, OVRInput.Controller.Touch);
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Blocks"))
         {
             colliding = true;
-            lastCollidingObject = other.rigidbody;
+            lastCollidingObject = other.gameObject.GetComponent<Rigidbody>();
             Debug.Log("Collision Enter");
         }
     }
 
-    private void OnCollisionStay(Collision other)
+    private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (colliding && OVRInput.GetDown(clickButton,  OVRInput.Controller.Touch))
         {
             pullable = lastCollidingObject;
             Debug.Log("Button Down");
-        } 
+        }
     }
 
-    private void OnCollisionExit(Collision other)
+    private void OnTriggerExit(Collider other)
     {
         colliding = false;
         Debug.Log("Collision Exit");
@@ -44,33 +63,33 @@ public class Rubberband : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (pullable && Input.GetKey(KeyCode.Space))
+        if (pullable && OVRInput.Get(clickButton,  OVRInput.Controller.Touch))
         {
             var position = this.transform.position;
             distance = Vector3.Distance(pullable.transform.position, position);
             var direction = position - pullable.position;
             Debug.Log(distance);
-        
-            pullable.AddForce(direction * distance, ForceMode.Force);
+
+            pullable.AddForce(direction * distance* 1000 *Time.deltaTime, ForceMode.Force);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-           // GetComponent<Rigidbody>().AddForce(1.0f, 0, 0, ForceMode.Force);
-           var xVector = new Vector3(0.04f, 0, 0);
-           transform.position += xVector;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            //GetComponent<Rigidbody>().AddForce(-1.0f, 0, 0, ForceMode.Force);
-            var xVector = new Vector3(-0.04f, 0, 0);
-            transform.position += xVector;
-        }
+        // if (Input.GetKey(KeyCode.RightArrow))
+        // {
+        //    // GetComponent<Rigidbody>().AddForce(1.0f, 0, 0, ForceMode.Force);
+        //    var xVector = new Vector3(0.04f, 0, 0);
+        //    transform.position += xVector;
+        // }
+        // if (Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     //GetComponent<Rigidbody>().AddForce(-1.0f, 0, 0, ForceMode.Force);
+        //     var xVector = new Vector3(-0.04f, 0, 0);
+        //     transform.position += xVector;
+        // }
     }
 
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (OVRInput.GetUp(clickButton,  OVRInput.Controller.Touch))
         {
             Debug.Log("Space UP");
             pullable = null;
